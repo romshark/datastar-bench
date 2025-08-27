07eecea0cf0fd075463cff43346a3b0c53e93bed
Roman Sharkov <roman.scharkov@gmail.com>
2025-08-28 00:59:21 +0200

    test: Add HTML vs JSON benchmarks

--- go.mod
+++ go.mod
@@ -5,11 +5,17 @@ go 1.25.0
 require (
 	github.com/a-h/templ v0.3.943
 	github.com/andybalholm/brotli v1.2.0
+	github.com/bytedance/sonic v1.14.0
 	github.com/starfederation/datastar-go v1.0.2
 )
 
 require (
 	github.com/CAFxX/httpcompression v0.0.9 // indirect
+	github.com/bytedance/sonic/loader v0.3.0 // indirect
+	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/bytedance/sonic"
 	"github.com/romshark/datastar-bench/template"
 )
 
@@ -30,6 +33,45 @@ func TestFatMorphDataJSON(t *testing.T) {
 		err = json.Unmarshal(fc, &data)
 		if err != nil {
 			t.Fatal("unexpected JSON unmarshaling error:", err)
+		}
+	}
+}
+
+func BenchmarkChatHTML(b *testing.B) {
+	var buffer bytes.Buffer
+	buffer.Grow(8 * 1024)
+	start := time.Now()
+	ctx := b.Context()
+	for b.Loop() {
+		buffer.Reset()
+		if err := template.FragSSEPatchFatMorphContent(
+			1, start, start, dataFatMorph2,
+		).Render(ctx, &buffer); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkChatJSONStd(b *testing.B) {
+	var buffer bytes.Buffer
+	buffer.Grow(8 * 1024)
+	for b.Loop() {
+		buffer.Reset()
+		err := json.NewEncoder(&buffer).Encode(dataFatMorph2)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkChatJSONSonic(b *testing.B) {
+	var buffer bytes.Buffer
+	buffer.Grow(8 * 1024)
+	for b.Loop() {
+		buffer.Reset()
+		err := sonic.ConfigFastest.NewEncoder(&buffer).Encode(dataFatMorph2)
+		if err != nil {
+			b.Fatal(err)
 		}
 	}
 }
